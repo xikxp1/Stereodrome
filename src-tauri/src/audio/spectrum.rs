0ba391962d823c04085eb5a7013c4c7879061ee0
Ivan Kachalkin <xikxp1@gmail.com>
2026-01-21 04:13:03 +0100

    chore: more gain for upper bands

--- src-tauri/src/audio/spectrum.rs
+++ src-tauri/src/audio/spectrum.rs
@@ -49,8 +49,8 @@ const BAND_FREQUENCIES: [(f32, f32); NUM_BANDS] = [
     (750.0, 1200.0),    // Mids
     (1200.0, 2000.0),   // Mids high
     (2000.0, 3200.0),   // Upper-mids
-    (3200.0, 4800.0),   // Presence
-    (4800.0, 6000.0),   // Brilliance low
+    (3200.0, 4600.0),   // Presence
+    (4600.0, 6000.0),   // Brilliance low
     (6000.0, 8000.0),  // Brilliance high
     (8000.0, 10000.0), // Highs
 ];
