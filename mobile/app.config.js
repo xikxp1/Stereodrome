a6d58f9f39bfed7454aa1fe68badd6862265fae6
Ivan Kachalkin <xikxp1@MacBook-Air-Kaygorodova.local>
2026-05-14 12:17:09 +0200

    chore: eas local build

---
+++ mobile/app.config.js
@@ -0,0 +1,27 @@
+const app = require("./app.json");
+
+module.exports = () => {
+  const expo = app.expo;
+  const extra = expo.extra || {};
+  const eas = extra.eas || {};
+  const ios = expo.ios || {};
+  const projectId = process.env.EAS_PROJECT_ID || eas.projectId;
+  const buildNumber = process.env.IOS_BUILD_NUMBER || ios.buildNumber || "1";
+
+  return {
+    ...expo,
+    ios: {
+      ...ios,
+      buildNumber,
+    },
+    extra: {
+      ...extra,
+      eas: projectId
+        ? {
+            ...eas,
+            projectId,
+          }
+        : eas,
+    },
+  };
+};
