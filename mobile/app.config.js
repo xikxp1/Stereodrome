ba9bc72d31483437a33ca09ebd20d34b9aaacdbc
Ivan Kachalkin <xikxp1@MacBook-Air-Kaygorodova.local>
2026-05-14 13:15:26 +0200

    fix build

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -5,11 +5,13 @@ module.exports = () => {
   const extra = expo.extra || {};
   const eas = extra.eas || {};
   const ios = expo.ios || {};
+  const slug = process.env.EAS_PROJECT_SLUG || expo.slug;
   const projectId = process.env.EAS_PROJECT_ID || eas.projectId;
   const buildNumber = process.env.IOS_BUILD_NUMBER || ios.buildNumber || "1";
 
   return {
     ...expo,
+    slug,
     ios: {
       ...ios,
       buildNumber,
